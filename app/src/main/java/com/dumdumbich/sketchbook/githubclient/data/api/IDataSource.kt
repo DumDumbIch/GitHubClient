6a81904bcec9017a31ea71ff3c5ac5bb9a878ae9
DumDumbIch <dumdumbich@gmail.com>
2021-03-25 17:58:27 +0300

    Add new Retrofit request

--- app/src/main/java/com/dumdumbich/sketchbook/githubclient/data/api/IDataSource.kt
+++ app/src/main/java/com/dumdumbich/sketchbook/githubclient/data/api/IDataSource.kt
@@ -1,12 +1,17 @@
 package com.dumdumbich.sketchbook.githubclient.data.api
 
+import com.dumdumbich.sketchbook.githubclient.domain.entity.GitHubRepository
 import com.dumdumbich.sketchbook.githubclient.domain.entity.GitHubUser
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface IDataSource {
 
     @GET("users")
     fun getUsers() : Single<List<GitHubUser>>
+
+    @GET("users/{user}/repos")
+    fun getUserRepositories(@Path("user") user: String):Single<List<GitHubRepository>>
 
 }
